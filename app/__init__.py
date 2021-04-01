80d24d12ee851cd01dd17bc0322126c91c274a1c
TreyBCollier <treycollier@treys-mbp.home>
2021-04-01 18:53:30 +0100

    index

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
+@app.route('/')
+def index():
+    return "Loading translation..."
+
+
 @app.route('/translateSent/<sentence>')
 def translateSent(sentence):
     return translate(sentence)
