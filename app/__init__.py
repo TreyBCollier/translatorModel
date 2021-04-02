03c6322e153cb18697df6946d3b4033c3ccc3635
TreyBCollier <treycollier@treys-mbp.home>
2021-04-02 14:33:01 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,4 @@ def index():
 
 @app.route('/translateSent/<sentence>')
 def translateSent(sentence):
-    translation = translate(sentence)
-    response = ""
-    if(translation):
-        response = translation
-    return response
+    return translate(sentence)
