72336decf9e520a02a05a64b25165a19a63abb92
TreyBCollier <treycollier@treys-mbp.home>
2021-04-12 18:38:33 +0100

    Updates and Comments

--- test.py
+++
@@ -1,9 +0,0 @@
-# from train import translateFrenchToEnglish
-from trainEnglishToFrench import translateEnglishToFrench
-
-# translation = translateFrenchToEnglish("J'aime le café au lait")
-
-translation = translateEnglishToFrench("I live in london")
-print(translation)
-
-print("DONE")
